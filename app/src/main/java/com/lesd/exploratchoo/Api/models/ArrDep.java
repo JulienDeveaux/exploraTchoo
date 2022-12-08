dc1f58800752458519596bf3418221c0d5f78edd
julienDeveaux <julien.deveaux@hotmail.com>
2022-12-08 21:34:53 +0100

    Feat : add links to SNCFResponse to use

--- app/src/main/java/com/lesd/exploratchoo/Api/models/ArrDep.java
+++ app/src/main/java/com/lesd/exploratchoo/Api/models/ArrDep.java
@@ -3,5 +3,6 @@ package com.lesd.exploratchoo.Api.models;
 public class ArrDep
 {
     public TrainInfos display_informations;
+    public LinksInfos[] links;
     public DateInfos stop_date_time;
 }
---
+++ app/src/main/java/com/lesd/exploratchoo/Api/models/LinksInfos.java
@@ -0,0 +1,7 @@
+package com.lesd.exploratchoo.Api.models;
+
+public class LinksInfos
+{
+    public String type;
+    public String id;
+}
